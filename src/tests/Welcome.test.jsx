c347b725cfb97fbcb5b66a2749f08d0ad6b5f367
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2025-01-16 14:18:39 +0100

    Es 1

--- src/tests/Welcome.test.jsx
+++ src/tests/Welcome.test.jsx
@@ -1,10 +1,16 @@
-import { describe, it } from "vitest";
+import { render, screen } from "@testing-library/react";
+import { describe, expect, it } from "vitest";
+import Welcome from "../components/Welcome";
 
-describe('',()=>{
-    it('',()=>{
+describe('Montaggio componente ',()=>{
+    it('montaggio welcome',()=>{
         // 1)
+        render(<Welcome/>)
         // 2)
-        // 3)
+         const h1 = screen.getByText(/Benvenuti in EpiBooks!/i)
+
+        // 3) NON presenti interazioni con la pagina
         // 4)
+        expect(h1).toBeInTheDocument()
     })
 })
