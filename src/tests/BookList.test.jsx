88a72bd76fa44713348a8db9cce9fb0b8d783668
Federico Tonti <148573975+Friedrich3@users.noreply.github.com>
2025-01-16 15:46:29 +0100

    Es.4

--- src/tests/BookList.test.jsx
+++ src/tests/BookList.test.jsx
@@ -1,5 +1,5 @@
 import fantasy from "../data/fantasy.json";
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { describe, expect, it } from "vitest";
 import BookList from "../components/BookList";
 
@@ -16,4 +16,54 @@ describe("Verifica che vengano effettivamente renderizzate tante bootstrap cards
     //4)
     expect(cards).toHaveLength(fantasy.length);
   });
+});
+
+//devo controllare che ci sia un searchbar
+//devo controllare se il numero di carte e' minore del numero di oggetti dentro al json
+//diversi test su carte
+
+describe("Verifica che il .filter avvenga correttamente", () => {
+  it("Controllo se la searchbar e presente", () => {
+    //1)
+    render(<BookList books={fantasy} />);
+    // 2)
+    const searchBar = screen.getByPlaceholderText(/cerca un libro/i);
+    // 3)
+    // 4)
+    expect(searchBar).toBeInTheDocument();
+  });
+
+  it('Controllo il numero di carte quando inserisco un input',()=>{
+     //1)
+     render(<BookList books={fantasy} />);
+     // 2)
+     const searchBar = screen.getByPlaceholderText(/cerca un libro/i);
+     // 3)
+     fireEvent.change(searchBar,{target: { value:'witcher'}} )
+     const cards1 = screen.getAllByRole('img');
+     // 4)
+     expect(cards1.length).toBeLessThan(fantasy.length)
+  });
+  it('Controllo il numero di carte quando inserisco un input',()=>{
+    //1)
+    render(<BookList books={fantasy} />);
+    // 2)
+    const searchBar = screen.getByPlaceholderText(/cerca un libro/i);
+    // 3)
+    fireEvent.change(searchBar,{target: { value:'warcraft'}})
+    const cards2 = screen.getAllByRole('img');
+    // 4)
+    expect(cards2.length).toBeLessThan(fantasy.length)
+ });
+ it('Controllo il numero di carte quando inserisco un input impossibile',()=>{
+  //1)
+  render(<BookList books={fantasy} />);
+  // 2)
+  const searchBar = screen.getByPlaceholderText(/cerca un libro/i);
+  // 3)
+  fireEvent.change(searchBar,{target: { value:'fgabnoigagag'}})
+  const cards2 = screen.queryAllByRole('img');
+  // 4)
+  expect(cards2).toHaveLength(0)
+})
 });
